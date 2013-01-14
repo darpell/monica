a1172541d688ff055c8b6bf1089e0b8b9c4b6b79
darpell <pelonia.darius@gmail.com>
2013-01-14 21:51:06 +0800

    changed ccs linking(header.php) and base url(config.php)
    changed line 7 in templates/header.php as it could not properly call
    the ccs. also added ../workspace/.. in the case url
    
    Signed-off-by: darpell <pelonia.darius@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/monica';
+$config['base_url']	= 'http://localhost/workspace/monica';
 $config['css'] = 'styles/style.css';
 
 /*
