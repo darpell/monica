ff5a5240d2079293a0a435eccc290e6145a2691c
christian <manuel.christian22@gmail.com>
2013-03-18 22:53:44 +0800

    added dashboard function for CHO users

--- application/models/cho_model.php
+++ application/models/cho_model.php
@@ -91,7 +91,59 @@
 			$query = $this->db->query($qString);
 			$query->free_result();
 		}
+		function get_barangay_count()
+		{
+			$qString = 'CALL ';
+			$qString .= "get_barangay_dashboard_data ('"; // name of stored procedure
+			$qString .=
+			date('y-m-d'). "'". ")";
+				
+			$q = $this->db->query($qString);
+
+			if($q->num_rows() > 0)
+			{	$data ='';
+			foreach ($q->result() as $row)
+			{	
+			$data .=
+			$row->ctr . "&&" .
+			$row->cr_barangay . "&&" .
+			$row->year . "%%" ;
+			}
+				
+			return $data;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		function get_age_count()
+		{
+			$qString = 'CALL ';
+			$qString .= "get_dashboard_data_age ('"; // name of stored procedure
+			$qString .=
+			date('y-m-d'). "'". ")"; 
+		
+			$q = $this->db->query($qString);
+		
+			if($q->num_rows() > 0)
+			{	$data ="";
+			foreach ($q->result() as $row)
+			{	
+				$data .=
+				$row->cr_barangay . "&&" .
+				($row->agerange * 10)."-".(($row->agerange *10)+10) . "&&" .
+				$row->patientcount . "%%" ;
+			}
+		
+			return $data;
+			}
+			else
+			{
+				return 0;
+			}
+		}
 	}
 
-/* End of case_report.php */
-/* Location: ./application/models/case_report.php */
+/* End of cho_model.php */
+/* Location: ./application/models/cho_model.php */
---
+++ application/views/pages/dashboard.php
@@ -0,0 +1,205 @@
+<!-- HEADER -->
+<?php $this->load->view('templates/header');?>
+
+
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load('visualization', '1.1', {packages: ['controls','corechart']});
+    </script>
+    <script type="text/javascript">
+      function drawVisualization() {
+    		var today = new Date();
+    		var dd = today.getDate();
+    		var mm = today.getMonth()+1;
+    		var yyyy = today.getFullYear() ;
+    		if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
+
+
+    	var str2 = document.getElementById('barangay_count').value.toString();
+
+		str2 = str2.split("%%");
+		var data = new Array();
+		for (var i = 0; i < str2.length; i++)
+		{
+			data[i] = str2[i].split("&&");
+		}
+		data.pop();
+			var m1 = 0;
+			var m2 = 0;
+			var m3 = 0;
+			var m4 = 0;
+    		
+			var mn1 = 0;
+			var mn2 = 0;
+			var mn3 = 0;
+			var mn4 = 0;
+	
+			for (var i = 0; i < data.length; i++)
+			{	
+				if(parseInt(data[i][2]) == parseInt(yyyy))
+				{
+				if (data[i][1] =='LANGKAAN II' ) 
+				{mn1 = data[i][0];}
+				if (data[i][1] == 'SAMPALOC I') 
+				{mn2 = data[i][0];}
+				if (data[i][1] == 'SAN AGUSTIN I') 
+				{mn3 = data[i][0];}
+				if (data[i][1] == 'SAN AGUSTIN III') 
+				{mn4 = data[i][0];}
+				}
+				else if(parseInt(data[i][2]) == parseInt(yyyy-1))
+				{
+				if (data[i][1] == 'LANGKAAN II') 
+				{m1 = data[i][0];}
+				if (data[i][1] == 'SAMPALOC I') 
+				{m2 = data[i][0];}
+				if (data[i][1] == 'SAN AGUSTIN I') 
+				{m3 = data[i][0];}
+				if (data[i][1] == 'SAN AGUSTIN III') 
+				{m4 = data[i][0];}
+				}
+			}
+ 
+          // Prepare the data
+        var str = document.getElementById('age_count').value.toString();
+
+					str = str.split("%%");
+					var data2 = new Array();
+					for (var i = 0; i < str.length; i++)
+					{
+						data2[i] = str[i].split("&&");
+					}
+					data2.pop();
+		 var datatable = new google.visualization.DataTable();
+		 datatable.addColumn('string', 'Barangay');
+		 datatable.addColumn('string', 'Age Group');
+		 datatable.addColumn('number', 'No. of Cases');
+		 datatable.addRows(data2.length);
+		 for (var i = 0; i < data2.length; i++)
+			{
+			 datatable.setCell(i, 0 , data2[i][0]);
+			 datatable.setCell(i, 1 , data2[i][1]);
+			 datatable.setCell(i, 2 , parseInt(data2[i][2]));
+			}
+
+   
+        var categoryPicker = new google.visualization.ControlWrapper({
+          'controlType': 'CategoryFilter',
+          'containerId': 'control2',
+          'options': {
+            'filterColumnLabel': 'Age Group',
+            'ui': {
+            'labelStacking': 'vertical',
+              'allowTyping': false,
+              'allowMultiple': false
+            }
+          }
+        });
+        var categoryPicker2 = new google.visualization.ControlWrapper({
+            'controlType': 'CategoryFilter',
+            'containerId': 'control3',
+            'options': {
+              'filterColumnLabel': 'Barangay',
+              'ui': {
+              'labelStacking': 'vertical',
+                'allowTyping': false,
+                'allowMultiple': false
+              }
+            }
+          });
+      
+        // Define a table
+        var table = new google.visualization.ChartWrapper({
+          'chartType': 'Table',
+          'containerId': 'chart2',
+          'options': {
+           
+          }
+        });
+        var pie = new google.visualization.ChartWrapper({
+            'chartType': 'PieChart',
+            'containerId': 'chart1',
+            'options': {
+              'width': 300,
+              'height': 300,
+              'legend': {position: 'top', textStyle: {color: 'blue', fontSize: 16}},
+              
+              'title': 'No. of cases',
+              'chartArea': {'left': 15, 'top': 15, 'right': 0, 'bottom': 0},
+              'pieSliceText': 'label'
+            },
+            // from the 'data' DataTable.
+            'view': {'columns': [0, 2]}
+          });
+
+      
+ 
+        // Create a dashboard
+        new google.visualization.Dashboard(document.getElementById('dashboard')).
+            // Establish bindings, declaring the both the slider and the category
+            // picker will drive both charts.
+            bind([categoryPicker,categoryPicker2], [pie,table]).
+            // Draw the entire dashboard.
+            draw(datatable);
+
+         var options = {
+                title : 'This month dengue cases by barangay compared to the previous year',
+                vAxis: {title: "Number Of Cases"},
+                hAxis: {title: "Barangay"},
+                seriesType: "bars",
+                series: {5: {type: "line"}}
+              };
+        var data = google.visualization.arrayToDataTable([
+       	['Barangay', yyyy, yyyy-1],
+        ['Langkaan II',  parseInt(mn1),      parseInt(m1)],
+        ['Sampaloc I',  parseInt(mn2),      parseInt(m2)],
+        ['San Agustin I',  parseInt(mn3),     parseInt(m3)],
+        ['San Agustin III',  parseInt(mn4),      parseInt(m4)]
+        ]);
+              // Instantiate and draw our chart, passing in some options.
+              var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+              chart.draw(data, options);
+
+      }
+
+      google.setOnLoadCallback(drawVisualization);
+    </script>
+  <div class="body">
+   <center>
+   <h3>Dengue Cases Summary</h3>
+    <div id="dashboard">
+      <table>
+        <tr style='vertical-align: top'>
+          <td style='width: 300px; font-size: 0.9em;'>
+            <div id="control1"></div>
+            <div id="control2"></div>
+            <div id="control3"></div>
+          </td>
+          <td>
+            <div style="float: left;" id="chart1"></div>
+            <div style="float: left;" id="chart2"></div>
+            <div style="float: left;" id="chart3"></div>
+          </td>
+        </tr>
+      </table>
+    </div>
+   <div id='chart_div'></div>
+   
+   
+</center>
+<?php 
+$attributes = array(
+						'id' => 'TPcr-form'
+					);
+echo form_open('CHO/view_tasks',$attributes); ?>
+
+		<div class="blog">
+
+<input type="hidden" name="table_data" id="table_data" value="<?php echo $table_data; ?>" />
+<input type="hidden" name="barangay_count" id="barangay_count" value="<?php echo $barangay_count; ?>" />
+<input type="hidden" name="age_count" id="age_count" value="<?php echo $age_count; ?>" />
+</div>
+
+
+<?php $this->load->view('templates/footer'); ?>
+</html>
