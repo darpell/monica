d69c1475f3746af9e95f8ce1755cec32a33effe9
christian <manuel.christian22@gmail.com>
2013-03-26 12:00:36 +0800

    fixed links postion

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -12,66 +12,6 @@
 					<h2> Monitor cases. <br><br> Check places. <br><br> <span>Save Lives.</span></h2>
 				</div>
 			</div>
-			<div class="section">
-				<div>
-					<div>
-						<a href="programs.html"><img src="<?= base_url('/images/help-out.jpg'); ?>" alt=""></a>
-						<p>
-							This website template has been designed by Free Website Templates for you, for free. You can replace all this text with your own text.
-						</p>
-					</div>
-					<div>
-						<h3><a href="programs.html">Top Programs</a></h3>
-						<ul>
-							<li>
-								<a href="programs.html"><img src="<?= base_url('/images/top-program1.jpg'); ?>" alt=""></a>
-								<h4><a href="programs.html">Tree Planting</a></h4>
-								<p>
-									If you're having problems editing this website template.
-								</p>
-							</li>
-							<li>
-								<a href="programs.html"><img src="<?= base_url('/images/top-program2.jpg'); ?>" alt=""></a>
-								<h4><a href="programs.html">Paper Recycling</a></h4>
-								<p>
-									Then don't hesitate to ask for help on the.
-								</p>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div>
-					<div>
-						<a href="programs.html"><img src="<?= base_url('/images/what-we-do.jpg'); ?>" alt=""></a>
-						<p>
-							You can remove any link to our website from this website template, you're free to use this website template without linking back to us.
-						</p>
-					</div>
-					<div>
-						<h3><a href="blog.html">Recent Blog Posts</a></h3>
-						<ul>
-							<li>
-								<a href="blog.html"><img src="<?= base_url('/images/blog-post1.jpg'); ?>" alt=""></a>
-								<div>
-									<h4><a href="blog.html">Blog Post Title One</a></h4>
-									<p>
-										You're free to use this website template without linking back to us.If you're having problems editing this website template, then don't hesitate to ask for help on the Forums... <a href="blog.html" class="more">Read More</a>
-									</p>
-								</div>
-							</li>
-							<li>
-								<a href="blog.html"><img src="<?= base_url('/images/blog-post.jpg'); ?>" alt=""></a>
-								<div>
-									<h4><a href="blog.html">Blog Post Title Two</a></h4>
-									<p>
-										You can do a lot with them. You can modify them. You can use them to design websites for clients, so long as you agree with the Terms of Use. You can even remove all our... <a href="blog.html" class="more">Read More</a>
-									</p>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 <!-- FOOTER -->
