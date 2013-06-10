50c1ee61e8dfcc97c6442834359916de3cfc30b0
darpell <pelonia.darius@gmail.com>
2013-06-11 01:23:17 +0800

    icons now showing. will work on displaying no of cases per barangay per polygon. last polygon still not showing

--- application/views/pages/remap.php
+++ application/views/pages/remap.php
@@ -28,7 +28,7 @@ body {height:100%;margin:0;padding:0}
 		{
 			var mapProp = {
 				center: dasma,
-				zoom: 10,
+				zoom: 12,
 				mapTypeId: google.maps.MapTypeId.TERRAIN
 			};
 			var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
@@ -43,83 +43,7 @@ body {height:100%;margin:0;padding:0}
 				cloudLayer.setMap(map);
 			/** end of Weather and Clouds Layer **/
 				
-			var oms = new OverlappingMarkerSpiderfier(map);
-			
-			var mcOptions = {gridSize: 50, maxZoom: 15};
 			var markers = [];
-
-				// Arrays for distance formula
-			var tracking_no = [];
-			var refNumber = [];
-			var household = [];
-			var container = [];
-
-			
-			var amount_200 = [];
-			var percent_200 = [];
-			var amount_50 = [];
-			var percent_50 = [];
-			
-			if (document.getElementById("result_length").value != 0)
-			{
-				for (var ctr = 0; ctr < document.getElementById("result_length").value; ctr++)
-				{
-					// distance formula necessities
-					tracking_no[ctr] = document.getElementById("pt_tracking_no" + ctr).value;
-			  		refNumber[ctr] = document.getElementById("pt_ref_no" + ctr).value;
-			  		household[ctr] = document.getElementById("pt_household" + ctr).value;
-			  		container[ctr] = document.getElementById("pt_container" + ctr).value;
-			  		/*amount_200[ctr] = document.getElementById("amount_200_" + ctr).value;
-					percent_200[ctr] = document.getElementById("percentage_200_" + ctr).value;
-			  		amount_50[ctr] = document.getElementById("amount_50_" + ctr).value;
-					percent_50[ctr] = document.getElementById("percentage_50_" + ctr).value;*/
-			  			
-					// end distance formula necessities
-				}
-			
-				for (var pt_ctr = 0; pt_ctr < document.getElementById("result_length").value; pt_ctr++) 
-				{
-					var marker = new google.maps.Marker({
-										position:new google.maps.LatLng(
-										document.getElementById("pt_lat" + pt_ctr).value,
-										document.getElementById("pt_lng" + pt_ctr).value
-								)
-						});
-	
-	
-					var householdcount = 0;
-					var containercount = 0;
-					for (var __i = 0; __i < household.length; __i++)
-			        {
-				       	if (household[pt_ctr] === household[__i])
-				       		householdcount++;
-				       	if (container[pt_ctr] === container[__i])
-				       		containercount++;
-					}
-					var householdpercent = householdcount / household.length * 100;
-					var containerpercent = containercount / container.length * 100;
-						
-					// infowindow content
-					var html = "<b>Larval Survey Report #: </b>" + refNumber[pt_ctr]
-				    	+ " <br/>" + "<b>Tracking #: </b>" + tracking_no[pt_ctr]
-				    /*	+ " <br/>" + "<b>Larval positives (LP) within: </b>"
-				    	+ " <br/>" + "<b>200m:</b>" + amount_200[pt_ctr] +" ("+ percent_200[pt_ctr] +"% of displayed LP)"
-				    	+ " <br/>" + "<b>50m:</b>" + amount_50[pt_ctr] +" ("+ percent_50[pt_ctr] +"% of displayed LP)" */
-				    	+ "<br/><br/>" + "<b>Household: </b>" + household[pt_ctr] +" ("+ householdcount +" of "+ household.length +" total occurrences, "+ householdpercent.toFixed(2) + "%)"
-				    	+ " <br/>" + "<b>Container: </b>" + container[pt_ctr] +" ("+ containercount +" of "+ container.length +" total occurances, "+ containerpercent.toFixed(2) + "%)";
-					// end infowindow content
-					
-					marker.info = new google.maps.InfoWindow({
-								content: html
-							});
-						oms.addListener('click', function(marker) {
-							 marker.info.open(map, marker);
-						});
-						
-					oms.addMarker(marker);
-					markers.push(marker);
-				} 
-			}
 
 			/** Map Nodes **/
 			//var node_markers*= [];
@@ -134,17 +58,23 @@ body {height:100%;margin:0;padding:0}
 											document.getElementById("nd_lat" + ctr).value,
 											document.getElementById("nd_lng" + ctr).value		
 										),
+										map: map,
 										icon: image
 							});
 	
-					node_marker.info = new google.maps.InfoWindow({
+					var node_marker_info = new google.maps.InfoWindow({
 								content: 'test'
 							});
-					oms.addListener('click', function(node_marker) {
+
+					google.maps.event.addListener(node_marker, 'click', function() {
+						node_marker_info.open(map,node_marker);
+						});
+					
+					/*oms.addListener('click', function(node_marker) {
 							node_marker.info.open(map, node_marker);
 						});
 						
-					oms.addMarker(node_marker);
+					oms.addMarker(node_marker);*/
 					markers.push(node_marker);
 				}
 			}
@@ -195,11 +125,10 @@ body {height:100%;margin:0;padding:0}
 					polygon_marker.setMap(map);
 				}
 			}
-			alert('test');
+			
 			/** end Polygon **/
 					
-			var mc = new MarkerClusterer(map, markers, mcOptions);
-			//var mc_nodes = new MarkerClusterer(map, node_markers, mcOptions);
+			//var mc = new MarkerClusterer(map, markers, mcOptions);
 		}
 		google.maps.event.addDomListener(window, 'load', initialize);
 	</script>
